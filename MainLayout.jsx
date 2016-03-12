f1fcbb0c8c4bd4ec2f40583bf938d4e001991129
holkiew <olkiewiczhubert@gmail.com>
2016-03-12 15:50:23 +0100

    Layout top strip
    dodany gorny pasek z logiem

--- MainLayout.jsx
+++ MainLayout.jsx
@@ -2,9 +2,19 @@ MainLayout = React.createClass({
   render() {
     return (
     <main>
-        {/*TODO Hubert: - zrobienie paska górnego z logiem prowadzącym do '/' */}
+		<body>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-10" id="topStrip">
+					<a href="localhost:3000">
+						<img src="img/LOGO.png" class="img-responsive" id="logo-img" />
+					</a>
+					</div>
+				</div>
+			</div>
+		</body>
+		{/*TODO Hubert: - zrobienie paska górnego z logiem prowadzącym do '/' */}
         {/*TODO Martin: - wstawienie interfejsu wyszukiwarki*/}
-        EBIP - layout główny (logo, linki, etc.)
         <AccountsUIWrapper />
         {this.props.content}
     </main>);
