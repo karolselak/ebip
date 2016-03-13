9e167bf69788d053ad2017950bbb324a684d585a
holkiew <olkiewiczhubert@gmail.com>
2016-03-13 23:22:13 +0100

    layout
    jakies poprawki

--- Homepage.jsx
+++ Homepage.jsx
@@ -8,29 +8,33 @@ Homepage = React.createClass({
     render()
     {
         return <div>
-            {this.renderInstitutions()}
-            <button type="button" className="btn btn-info" data-toggle="modal" data-target="#addInstitutionModal">Dodaj instytucję</button>
+            <div class="container">
+              <div class="row" id="button-addInstitutions">
+                <button type="button" className="btn btn-info" data-toggle="modal" data-target="#addInstitutionModal">Dodaj instytucję</button>
 
-            {/*okno dodawania instytucji: */}
-            <div className="modal fade" id="addInstitutionModal" role="dialog">
-            <div className="modal-dialog">
+                {/*okno dodawania instytucji: */}
+                <div className="modal fade" id="addInstitutionModal" role="dialog">
+                <div className="modal-dialog">
 
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <button type="button" className="close" data-dismiss="modal">&times;</button>
-                        <h4 className="modal-title">Modal Header</h4>
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <button type="button" className="close" data-dismiss="modal">&times;</button>
+                            <h4 className="modal-title">Modal Header</h4>
+                        </div>
+                    <div className="modal-body">
+                        <div>Nazwa instytucji:</div>
+                        <input type="text"></input>
                     </div>
-                <div className="modal-body">
-                    <div>Nazwa instytucji:</div>
-                    <input type="text"></input>
-                </div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn btn-success" data-dismiss="modal" onClick={this.addInstitution} >Dodaj</button>
-                        <button type="button" className="btn btn-default" data-dismiss="modal">Anuluj</button>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-success" data-dismiss="modal" onClick={this.addInstitution} >Dodaj</button>
+                            <button type="button" className="btn btn-default" data-dismiss="modal">Anuluj</button>
+                        </div>
                     </div>
                 </div>
-            </div>
-            </div>
+                </div>
+              {this.renderInstitutions()}
+          </div>
+        </div>
         </div>
     },
     renderInstitutions() {
--- MainLayout.jsx
+++ MainLayout.jsx
@@ -8,22 +8,28 @@ MainLayout = React.createClass({
 					<div class="col-md-10" id="topStrip">
 					<a href="localhost:3000">
 						<img src="img/LOGO.png" class="img-responsive" id="logo-img" />
-					</a>
+          </a>
+          <div class="col-md-2 col-md-push-2" id="signin">
+            <span id="AccountsUIWrapper"><AccountsUIWrapper /></span>
+          </div>
 					</div>
 				</div>
         <div class="row">
-          <div class="col-md-10">
-            <p id="AccountsUIWrapper"><AccountsUIWrapper /></p>
+          <div class="col-md-4">
+            {/*cos nie dziala... moze trzeba jakos podlinkowac zrodlo?*/}
+            <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+          </div>
+          <div class="col-md-10" id="browser">
+            <span class="wyszukiwarka">WYSZUKIWARKA</span>
           </div>
         </div>
         <div class="row">
-          <div class="col-md-10">
+          <div class="col-md-10" id="homepage">
               <Homepage />
           </div>
         </div>
 			</div>
 		</body>
-		{/*TODO Hubert: - zrobienie paska górnego z logiem prowadzącym do '/' */}
         {/*TODO Martin: - wstawienie interfejsu wyszukiwarki*/}
         {/*this.props.content*/}
     </main>);
