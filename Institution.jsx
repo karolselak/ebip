ef2f5feebb9288a6fc12aebb03ca53a00d12ac50
K.prez.S <karol.selak@gmail.com>
2016-03-15 20:59:33 +0100

    advanced article adding

--- Institution.jsx
+++ Institution.jsx
@@ -1,48 +1,91 @@
 Institution = React.createClass({
     mixins: [ReactMeteorData],
     getMeteorData() {
+        var ins = Institutions.findOne({name: this.props.institution});
         return {
-            articles: Articles.find().fetch()
+            articles: Articles.find({institution_id: ins && ins._id}).fetch(),
+            institution: ins
         };
     },
     render()
     {
-        {/*TODO Łukasz: wyświetlanie wszystkich artykułów (this.data.articles) oraz dodawanie nowych, analogicznie jak dzieje się to w Homepage.jsx*/}
-        return <div className="container" id="institution">
-            <button type="button" className="btn btn-info" data-toggle="modal" data-target="#addArticleModal">Dodaj artykuł</button>
+        return <div className='container' id='institution'>
+            <h2>{this.data.institution && this.data.institution.name}</h2>
+            <button type='button' className='btn btn-info' data-toggle='modal' data-target='#addArticleModal'>Dodaj artykuł</button>
 
+
+            {this.renderArticles()}
             {/*okno dodawania instytucji: */}
-            <div className="modal fade" id="addArticleModal" role="dialog">
-            <div className="modal-dialog">
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <button type="button" className="close" data-dismiss="modal">&times;</button>
-                        <h4 className="modal-title">Dodaj artykuł</h4>
+            <div className='modal fade' id='addArticleModal' role='dialog'>
+            <div className='modal-dialog'>
+                <div className='modal-content'>
+                    <div className='modal-header'>
+                        <button type='button' className='close' data-dismiss='modal'>&times;</button>
+                        <h4 className='modal-title'>Dodaj artykuł</h4>
                     </div>
-                    <div className="modal-body">
+                    <div className='modal-body'>
                         <div>Tytuł:</div>
-                        <input id='title' type="text"></input>
+                        <input id='title' className='form-control' type='text'></input>
                         <div>Treść:</div>
-                        <textarea id='content' rows='5' cols='80'></textarea>
+                        <textarea id='content' className='form-control' rows='5' cols='80'></textarea>
+                        <div>Tagi:</div>
+                        <input id='tags' type='text' className='form-control' placeholder='tag1, tag2...'></input>
+                        <div>Data publikacji:</div>
+                        <div className='input-group date' id='publicationDate'>
+                            <input type='text' className='form-control' placeholder='nieokreślona'/>
+                            <span className='input-group-addon'>
+                                <span className='glyphicon glyphicon-calendar'></span>
+                            </span>
+                        </div>
+                        <div>Data wygaśnięcia:</div>
+                        <div className='input-group date' id='expirationDate'>
+                            <input type='text' className='form-control' placeholder='nieokreślona'/>
+                            <span className='input-group-addon'>
+                                <span className='glyphicon glyphicon-calendar'></span>
+                            </span>
+                        </div>
                     </div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn btn-success" data-dismiss="modal" onClick={this.addArticle} >Dodaj</button>
-                        <button type="button" className="btn btn-default" data-dismiss="modal">Anuluj</button>
+                    <div className='modal-footer'>
+                        <button type='button' id='pbtn' className='btn btn-success' data-dismiss='modal' onClick={this.addArticle} >Publikuj</button>
+                        <button type='button' id='sbtn' className='btn btn-info' data-dismiss='modal' onClick={this.addArticle} >Zapisz wersję roboczą</button>
+                        <button type='button' className='btn btn-default' data-dismiss='modal'>Anuluj</button>
                     </div>
                 </div>
             </div>
             </div>
-            {this.renderArticles()}
         </div>
     },
     addArticle(event) {
         var $modal = $(event.target).closest('.modal-content');
-        var title = $modal.find('#title')[0];
-        var content = $modal.find('#content')[0];
+        var $title = $modal.find('#title')[0];
+        var $content = $modal.find('#content')[0];
+        var $tags = $modal.find('#tags')[0];
+        var expirationDate = $modal.find('#expirationDate').data('DateTimePicker').date();
+        if (event.target.id == 'pbtn') {
+            var publicationDate = (new Date()).getTime();
+        } else {
+            var d = $modal.find('#publicationDate').data('DateTimePicker').date();
+            var publicationDate = d ? d._d.getTime() || Infinity;            
+        }
         Meteor.call('addArticle', {
-            title: title.value,
-            content: content.value
+            title: $title.value,
+            content: $content.value,
+            institution_id: this.data.institution._id,
+            tags: $tags.value ? $tags.value.split(', ') : [],
+            publicationDate: publicationDate
         })
+    },
+    componentDidMount(){
+        $(function () {
+            var $pd = $('#publicationDate');
+            var $ed = $('#expirationDate');
+            $pd.datetimepicker({locale: 'pl'});
+            $pd.data('DateTimePicker').minDate(new Date());
+            $pd.data('DateTimePicker').clear();
+            $ed.datetimepicker({locale: 'pl'});
+            $ed.data('DateTimePicker').minDate(new Date());
+            $ed.data('DateTimePicker').clear();
+        });
     },
     renderArticles() {
         return this.data.articles.map(function(el){
