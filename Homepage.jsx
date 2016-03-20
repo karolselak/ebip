55a51c1c65154f41e61e10ef0a9482bb175faf70
KajP <kaj.23@op.pl>
2016-03-20 12:38:41 +0100

    Praca nad usuwaniem instytucji

--- Homepage.jsx
+++ Homepage.jsx
@@ -41,7 +41,43 @@ Homepage = React.createClass({
     },
     renderInstitutions() {
         return this.data.institutions.map(function(el){
-            return <div><a href={'/'+el.name}>{el.name}</a></div>
+            return <div>
+                <a href={'/'+el.name}>{el.name}</a>
+                <button type="button" className="btn btn-danger"
+                data-toggle="modal" data-target="#removeInstitutionModal">
+                    Usuń instytucję {el.name}
+                </button>
+
+                <div className="modal fade" id="removeInstitutionModal"
+                role="dialog">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <button type="button" className="close"
+                                data-dismiss="modal">&times;</button>
+                                <h4 className="modal-title">
+                                Usuń instytucję
+                                </h4>
+                            </div>
+                            <div className="modal-body">
+                                Czy chcesz usunąć instytucję {el.name}?
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button"
+                                className="btn btn-primary" data-dismiss="modal"
+                                onClick={this.removeInstitution}>
+                                    Usuń
+                                </button>
+                                <button type="button"
+                                className="btn btn-secondary"
+                                data-dismiss="modal">
+                                    Anuluj
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         })
     },
     addInstitution(event) {
@@ -49,5 +85,8 @@ Homepage = React.createClass({
         Meteor.call('addInstitution', {
             name: name.value
         })
+    },
+    removeInstitution(event) {
+
     }
 });
