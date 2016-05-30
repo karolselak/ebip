f66906620f4812ca95fcfad009fbad1c05d8e899
Karol <karol.selak@gmail.com>
2016-05-30 06:47:25 +0200

    article edit

--- MainLayout.jsx
+++ MainLayout.jsx
@@ -52,7 +52,6 @@ MainLayout = React.createClass({
                     <div className="col-md-12 col-xs-12 col-sm-12 col-lg-12" id="hompeCont">
                       {this.props.content}
                     </div>
-                  </div>
                 </div>
 
                 <footer className="row">
